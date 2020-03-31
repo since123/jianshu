9cd007706ac0bd99e47f5df8e6eacc0d859f8ece
txj <1534147975@qq.com>
2020-03-31 19:05:58 +0800

    起步

---
+++ src/common/header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    HeaderWrapper
+ } from './style'
+
+class Header extends Component {
+  render(){
+    return (
+      <HeaderWrapper>sdfg</HeaderWrapper>
+    )
+  }
+}
+export default(
+  Header
+)
